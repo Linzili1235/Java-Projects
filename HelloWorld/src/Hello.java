89c939720c5f395cb7848c874cb162046db6c668
Linzili1235 <jq138@georgetown.edu>
2022-10-01 17:49:25 -0400

    sample commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Jingyi");
+
+        int myFirstNumber = 10 + 5;
+        int mySecondNumber = myFirstNumber;
+        myFirstNumber = 5;
+
+        System.out.println(mySecondNumber);
+
+    }
+
+}
+
